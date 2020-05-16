b1b60bbc8bc7558cf03b13a125256b7b218100dc
suganuma-koji <suganuma_koji@r.recruit.co.jp>
2020-05-16 18:34:11 +0900

    test

--- google_compute_instance.tf
+++ google_compute_instance.tf
@@ -4,8 +4,8 @@ variable "PROJECT_ID" {}
 provider "google" {
   credentials = "${var.GOOGLE_CREDENTIALS}"
   project     = "${var.PROJECT_ID}"
-  region      = "us-central1"
-  zone        = "us-central1-c"
+  region      = "asia-northeast1"
+  zone        = "asia-northeast1-b"
 }
 
 resource "google_compute_instance" "tf-cloud-01" {
